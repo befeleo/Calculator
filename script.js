b133732d1c9f34eb622e59eef0d27a7eadd4c5bd
jerriker <befikerkassahuntessema@gmail.com>
2025-11-12 18:42:43 +0300

    add calculator function

---
+++ script.js
@@ -0,0 +1,10 @@
+const calculator = {
+    add(a, b) { return a + b },
+    subtract(a, b) { return a - b },
+    multiply(a, b) { return a * b },
+    divide(a, b) {
+        if (b === 0)
+            return "Error: Can't divide 0"
+        return a / b
+    }
+}
