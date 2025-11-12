47f91009377c5676fec8db3a9f0632cf26cdf7f2
jerriker <befikerkassahuntessema@gmail.com>
2025-11-12 19:50:22 +0300

    add calculate function

--- script.js
+++ script.js
@@ -8,4 +8,26 @@ let operator = null
 const calculateDisplay = (value) => {
     display.textContent = value
 }
+const calculate = (a, operator, b) => {
+
+    switch (operator) {
+        case '+':
+            return a + b
+            break;
+        case '-':
+            return a - b
+            break;
+        case '*':
+            return a * b
+            break;
+        case '/':
+            if (b === 0)
+                return "Error"
+            return a / b
+            break;
+        default:
+            break;
+    }
+
+}
 
